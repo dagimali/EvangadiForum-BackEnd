14c4f9c59408ce3ad591a8a60b537238fccd2e20
dagimali <131529289+dagimali@users.noreply.github.com>
2023-07-31 19:50:05 -0400

    header removed from Server.js

--- Server.js
+++ Server.js
@@ -20,15 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set up headers to allow cross-origin requests and custom headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+//   );
+//   next();
+// });
 
 // Mount the userRouter for handling /api/users routes
 app.use("/api/users", userRouter);
