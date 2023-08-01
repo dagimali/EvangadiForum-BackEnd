61d4129ab4bb80633b05167cc4f71d0beffc69b4
dagimali <131529289+dagimali@users.noreply.github.com>
2023-08-01 01:07:22 -0400

    errors corrected

--- Server.js
+++ Server.js
@@ -1,48 +1,30 @@
 /** @format */
-
+// Require and configure dotenv to load environment variables from a .env file
 require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
 const userRouter = require("./Server/API/Users/user.router");
-
 const app = express();
-
-// 1. Load environment variables with dotenv
-app.use(cors({ origin: "*" }));
-
-// 2. Enable CORS with the cors middleware
+app.use(cors());
 app.use(express.json());
-
-// 3. Parse incoming JSON payloads
 app.use(express.urlencoded({ extended: true }));
 
-// 4. Parse incoming URL-encoded payloads
-
-// 5. Set up custom CORS headers middleware
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "https://16bf9250.evangadiforum-frontend-4re.pages.dev",
-    // Add other allowed origins here if needed
-  ];
-
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+// Set up headers to allow cross-origin requests and custom headers
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+//   );
+//   next();
+// });
 
 // 6. Mount the userRouter for handling /api/users routes
 app.use("/api/users", userRouter);
 
-const port = process.env.PORT || 8080;
+const port = process.env.port || 8080;
 
 app.listen(port, "0.0.0.0", (err) => {
   if (err) {
