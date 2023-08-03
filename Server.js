d925449f566a951612bdfd7dcad33dfb4fe31ba3
dagimali <131529289+dagimali@users.noreply.github.com>
2023-08-03 11:12:32 -0400

    heasder added

--- Server.js
+++ Server.js
@@ -14,16 +14,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set up headers to allow cross-origin requests and custom headers
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+//   );
+//   next();
+// });
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://06541ee4.evangadiforum-frontend-4re.pages.dev"
   );
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-
 // 6. Mount the userRouter for handling /api/users routes
 app.use("/api/users", userRouter);
 
